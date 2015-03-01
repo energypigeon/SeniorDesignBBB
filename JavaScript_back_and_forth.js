02dee8e693b86b522e42afaa4b3482dd3eae2c41
energypigeon <EnergyPigeon@01010110.local>
2015-02-28 23:01:30 -0700

    This is the updated version of "back and forth" in javascript

--- JavaScript_back_and_forth.js
+++ JavaScript_back_and_forth.js
@@ -1,9 +1,9 @@
 var b = require('bonescript');
 
-int dirpin = ;
-int steppin = ;
-int enable = ;
-
+var dirpin = "P9_11";
+var steppin = "P9_13";
+var enable = "P9_15";
+var state = b.LOW;              // Start the IO at a LO state
 
 b.pinMode(dirpin, b.OUTPUT);
 b.pinMode(steppin, b.OUTPUT);
@@ -12,26 +12,38 @@ var i = 0;
 
  loop();
 
-
  function loop(){
   
   b.digitalWrite(dirpin, b.HIGH);    // sets direction
-  setInterval(toggle, 1000);
+  setInterval(toggledir, 1000);
 
   for (i = 0; i<1000; i++)       // 800 microsteps is one revolution; 4 micro step is one step
   {
     b.digitalWrite(steppin, b.LOW);  // This LOW to HIGH change is what creates the
     b.digitalWrite(steppin, b.HIGH); // rising edge so that the driver knows to when to step.
-    setInterval(500);      
+    setInterval(togglestep, 500);      
   }                              
   
   b.digitalWrite(dirpin, b.LOW);    // sets direction
-  setInterval(1000);
+  setInterval(toggledir, 1000);
 
   for (i = 0; i<1000; i++)       // 800 microsteps is one revolution; 4 micro step is one step
   {
     b.digitalWrite(steppin, b.LOW);  // This LOW to HIGH change is what creates the
     b.digitalWrite(steppin, b.HIGH); // rising edge so that the driver knows to when to step.
-    setInterval(500);      
+    setInterval(togglestep, 500);      
   }  
 }
+
+function toggledir() {
+    if(state == b.LOW) state = b.HIGH;
+    else state = b.LOW;
+    b.digitalWrite(dirpin, state);
+}
+
+function togglestep() {
+    if(state == b.LOW) state = b.HIGH;
+    else state = b.LOW;
+    b.digitalWrite(steppin, state);
+}
+
